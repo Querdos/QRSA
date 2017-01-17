493e2a102523f550e53007f27bc141c190760ff6
Hamza ESSAYEGH <hamza.essayegh@gmail.com>
2017-01-17 14:40:04 +0100

    Updated decryption function for big text

--- main.c
+++ main.c
@@ -18,6 +18,292 @@
 #define BASE_SAVE 		61
 #define MAX_CHARS_LINES 50
 
+/**
+ * Encrypt a given file with a pre-saved public key
+ */
+void encrypt_file(char *filename_plain) {
+	// vars
+	FILE *fp_plain, *size_of_file, *fp_rsa;
+	mpz_t n, e;
+	unsigned char **encrypted, **plain;
+	unsigned long size, max_line;
+	char command_wc[11], count[3], ch_plain;
+	int k, i, j, nb_elements, step, status_pubkey;
+	
+	// generating the key pair
+	status_pubkey = load_pub(n, e);
+	if (status_pubkey == -1) {
+		exit(1);
+	}
+	k = mpz_size(n) * GMP_LIMB_BITS / 8;
+	
+	// opening the file (not encrypted)
+	fp_plain = fopen(filename_plain, "r");
+	
+	// exception
+	if (NULL == fp_plain) {
+		printf("Unable to open the file '%s'\n", filename_plain);
+		mpz_clears(n, e, NULL);
+		exit(1);
+	}
+
+	// retreiving size of *plain
+	sprintf(command_wc, "wc -c < %s", filename_plain);
+	size_of_file = popen(command_wc, "r");
+	
+	fgets(count, 10, size_of_file);
+	pclose(size_of_file);
+	size = atoi(count);
+	
+	// message length can be a maximum of (k-11)
+	// allocating memory for plain
+	nb_elements = 0;
+	plain = malloc((((size-size%(k-11))/(k-11)) + 1) * sizeof(*plain));
+	if (NULL == plain) {
+		printf("Memory error.\n");
+		mpz_clears(n, e, NULL);
+		exit(1);
+	}
+	
+	for (i=0; i<((size-size%(k-11))/(k-11)); i++) {
+		plain[i] = malloc((k-11) * sizeof(plain[i]));
+		if (NULL == plain[i]) {
+			printf("Memory error\n");
+			mpz_clears(n, e, NULL);
+			exit(1);
+		}
+		
+		memset(plain[i], '\0', (k-11));
+		nb_elements++;
+	}
+	
+	plain[i] = malloc((size % (k-11)) * sizeof(plain[i]));
+	if (NULL == plain[i]) {
+		printf("Memory error\n");
+		mpz_clears(n, e, NULL);
+		exit(1);
+	}
+	memset(plain[i], '\0', size % (k-11));
+	nb_elements++;
+	
+	// retrieving plain text
+	i = -1;
+	step = 0;
+	while (1) {
+		i++;
+		if (step != (nb_elements - 1)) {
+			if (i < (k-11)) {
+				plain[step][i] = fgetc(fp_plain);
+			} else {
+				i = -1;
+				step++;
+				continue;
+			}
+		} else {
+			ch_plain = fgetc(fp_plain);
+			if (feof(fp_plain)) {
+				fclose(fp_plain);
+				break;
+			} else {
+				plain[step][i] = ch_plain;
+			}
+		}
+	}
+	
+	// encrypting
+	encrypted = malloc((((size-size%(k-11))/(k-11)) + 1) * sizeof(*encrypted));
+	if (NULL == encrypted) {
+		printf("Memory error");
+		mpz_clears(n, e, NULL);
+		exit(1);
+	}
+	
+	fp_rsa = fopen("encrypted", "w");
+	for (i=0; i<nb_elements; i++) {
+		encrypted[i] = malloc(k * sizeof(encrypted[i]));
+		if (NULL == encrypted[i]) {
+			printf("Memory error.\n");
+			mpz_clears(n, e, NULL);
+			exit(1);
+		}
+		memset(encrypted[i], '\0', k);
+		
+		encrypted[i] = rsaes_pkcs1_encrypt(n, e, plain[i]);
+		free(plain[i]);
+		if (NULL == encrypted[i]) {
+			free(plain);
+			free(encrypted);
+			mpz_clears(n, e, NULL);
+			exit(1);
+		}
+		
+		// write
+		for (j=0; j<k; j++) {
+			fputc(encrypted[i][j], fp_rsa);
+		}
+		
+		free(encrypted[i]);
+	}
+	fclose(fp_rsa);
+	
+	// clearing pub key
+	mpz_clears(n, e, NULL);
+	free(plain);
+	free(encrypted);
+}
+
+/**
+ * Decrypt a given file with a pre-saved private key
+ */
+void decrypt_file(char *filename_encrypted) {
+	// vars
+	FILE *chars_count, *fp_encrypted, *fp_rsa;
+	unsigned char **encrypted, **decrypted;
+	char *wc_command, chars_result[5];
+	mpz_t n, d;
+	int chars, i, j, k, nb_decrypt;
+	
+	// retrieving rsa key pair (private)
+	if (load_priv(n, d) == -1) {
+		mpz_clears(n, d, NULL);
+		exit(1);
+	}
+	k = mpz_size(n) * GMP_LIMB_BITS / 8;
+	
+	// trying to open the file
+	fp_encrypted = fopen(filename_encrypted, "r");
+	if (NULL == fp_encrypted) {
+		printf("File doesn't exists. Aborting.\n");
+		exit(1);
+	}
+	
+	// number of chars in encrypted file
+	wc_command = malloc((strlen(filename_encrypted) + 8) * sizeof(char *));
+	sprintf(wc_command, "wc -c < %s", filename_encrypted);
+	
+	chars_count = popen(wc_command, "r");
+	free(wc_command);
+	fgets(chars_result, 5, chars_count);
+	pclose(chars_count);
+	chars = atoi(chars_result);
+	
+	fp_rsa = fopen("decrypted", "w");
+	if (NULL == fp_rsa) {
+		printf("Unable to open a file for decryption. Aborting.\n");
+		exit(1);
+	}
+	
+	if (chars > k) {
+		// how many times we will decrypt
+		nb_decrypt = chars / k;
+		
+		// allocating
+		encrypted = malloc(nb_decrypt * sizeof(*encrypted));
+		decrypted = malloc(nb_decrypt * sizeof(*decrypted));
+		if (NULL == encrypted) {
+			printf("Memory error.\n");
+			exit(1);
+		}
+		
+		for (i=0; i<nb_decrypt; i++) {
+			// allocating
+			encrypted[i] = malloc(k * sizeof(encrypted[i]));
+			if (NULL == encrypted[i]) {
+				printf("Memory error.\n");
+				exit(1);
+			}
+			
+			// retrieving first part
+			for (j=0; j<k; j++) {				
+				encrypted[i][j] = fgetc(fp_encrypted);
+			}
+			
+			decrypted[i] = rsads_pkcs1_decrypt(n, d, k, encrypted[i]);
+			free(encrypted[i]);
+			if (NULL == decrypted[i]) {
+				exit(1);
+			}
+			
+			for (j=0; j<strlen(decrypted[i]); j++) {
+				fputc(decrypted[i][j], fp_rsa);
+			}
+			free(decrypted[i]);
+		}
+	} else {
+		// allocating
+		encrypted = malloc(sizeof(*encrypted));
+		encrypted[0] = malloc(k * sizeof(encrypted[0]));	
+		
+		// TODO
+		
+		free(encrypted[0]);
+		free(encrypted);
+	}
+	
+	fclose(fp_encrypted);
+	fclose(fp_rsa);
+	
+}
+
+/**
+ * Save a key pair (public and private key) into .rsa directory 
+ */
+void key_pair() {
+	int dir_exists;
+	mpz_t n, e, d;
+	
+	// checking .rsa existance
+	dir_exists = mkdir(".rsa", 0755);
+	
+	// .rsa exists
+	if (-1 == dir_exists) {
+		char user_choice;
+		printf("Directory exists. Generate new key pair? [y|n] ");
+		user_choice = getchar();
+		
+		if (user_choice != 'y' && user_choice != 'n') {
+			printf("Aborting.\n");
+			exit(1);
+		}
+		
+		if ('y' == user_choice) {
+			// init
+			mpz_inits(n, e, d, NULL);
+			
+			// generating key pair
+			printf("Generating key pair...");
+			generate_keypair(n, e, d);
+			printf(" Done.\n");
+			
+			// saving
+			if (-1 == save_keypair(n, e, d)) {
+				mpz_clears(n, e, d, NULL);
+				exit(1);
+			}
+			
+			// cleaning
+			mpz_clears(n, e, d, NULL);
+		}
+	}
+	
+	// .rsa doesn't exists
+	else {
+		// inits
+		mpz_inits(n, e, d, NULL);
+		
+		// generating
+		printf("Generating key pair...");
+		generate_keypair(n, e, d);
+		printf(" Done.\n");
+		
+		// saving
+		save_keypair(n, e, d);
+		
+		// cleaning
+		mpz_clears(n, e, d, NULL);
+	}
+}
+
 int main(int argc, char** argv) {
 	// init time
 	srand(time(NULL));
@@ -30,212 +316,17 @@ int main(int argc, char** argv) {
 	
 	// for encryption
 	if (strcmp(argv[1], "--encrypt") == 0) {		
-		// vars
-		FILE *fp_plain, *size_of_file, *fp_rsa;
-		mpz_t n, e;
-		unsigned char *encrypted, *plain;
-		unsigned long size, max_line;
-		char command_wc[11], count[3], *filename;
-		int k, i;
-		
-		// generating the key pair
-		if (load_pub(n, e) == -1) {
-			return EXIT_FAILURE;
-		}
-		k = mpz_size(n) * GMP_LIMB_BITS / 8;
-		
-		// opening the file (not encrypted)
-		fp_plain = fopen(argv[2], "r");
-		
-		// exception
-		if (NULL == fp_plain) {
-			printf("Unable to open the file '%s'\n", argv[2]);
-			mpz_clears(n, e, NULL);
-			return EXIT_FAILURE;
-		}
-	
-		// retreiving size of *plain
-		sprintf(command_wc, "wc -c < %s", argv[2]);
-		size_of_file = popen(command_wc, "r");
-		
-		fgets(count, 10, size_of_file);
-		pclose(size_of_file);
-		size = atoi(count);
-		
-		// allocating memory for string
-		plain = (unsigned char *) malloc(size * sizeof(unsigned char *));
-		if (NULL == plain) {
-			printf("Memory error.\n");
-			return EXIT_FAILURE;
-		}
-		memset(plain, '\0', size);
-		
-		// retrieving plain text
-		i = 0;
-		while (1) {
-			// getting the line
-			plain[i++] = fgetc(fp_plain);
-			
-			if (feof(fp_plain)) {
-				break;
-			}
-		}
-		printf("%s\n", plain);
-		exit(1);
-		fclose(fp_plain);
-		
-		// encrypting
-		encrypted = rsaes_pkcs1_encrypt(n, e, plain);
-		free(plain);
-		mpz_clears(n, e, NULL);
-		if (NULL == encrypted) {
-			return EXIT_FAILURE;
-		}
-		
-		// encrypting plain text
-		filename = (char *) malloc(10 * strlen(argv[2]) * sizeof(char *));
-		memset(filename, '\0', sizeof(filename));
-		
-		// opening file
-		sprintf(filename, "%s.enc", argv[2]);
-		fp_rsa = fopen(filename, "w");
-		free(filename);
-		if (NULL == fp_rsa) {
-			printf("Error opening file for write operation. Aborting.\n");
-			return EXIT_FAILURE;
-		}
-		
-		// writing encrypted message
-		for (i=0; i<k; i++) {
-			fputc(encrypted[i], fp_rsa);
-		}
-		
-		// cleaning
-		free(encrypted);
-		fclose(fp_rsa);
+		encrypt_file(argv[2]);
 	}
 	
 	// for decryption
 	else if (strcmp(argv[1], "--decrypt") == 0) {
-		// vars
-		FILE *chars_count, *fp_encrypted, *fp_rsa;
-		unsigned char *encrypted, *decrypted;
-		char *wc_command, chars_result[5];
-		mpz_t n, d;
-		int chars, i;
-		
-		// retrieving rsa key pair (private)
-		if (load_priv(n, d) == -1) {
-			mpz_clears(n, d, NULL);
-			return EXIT_FAILURE;
-		}
-		
-		// trying to open the file
-		fp_encrypted = fopen(argv[2], "r");
-		if (NULL == fp_encrypted) {
-			printf("File doesn't exists. Aborting.\n");
-			return EXIT_FAILURE;
-		}
-		
-		// number of chars in encrypted file
-		wc_command = malloc((strlen(argv[2]) + 8) * sizeof(char *));
-		sprintf(wc_command, "wc -c < %s", argv[2]);
-		
-		chars_count = popen(wc_command, "r");
-		fgets(chars_result, 5, chars_count);
-		chars = atoi(chars_result);
-		
-		free(wc_command);
-		pclose(chars_count);
-		
-		// retreiving encrypted text
-		encrypted = malloc(chars * sizeof(unsigned char));
-		for (i=0; i<chars; i++) {
-			encrypted[i] = fgetc(fp_encrypted);
-		}
-		fclose(fp_encrypted);
-		
-		// decrypting
-		decrypted = rsads_pkcs1_decrypt(n, d, chars, encrypted);
-		mpz_clears(n, d, NULL);
-		free(encrypted);
-		if (NULL == decrypted) {
-			free(decrypted);
-			return EXIT_FAILURE;
-		}
-		
-		// writing to file
-		fp_rsa = fopen("decrypted", "w");
-		if (NULL == fp_rsa) {
-			printf("Unable to open a new file for operation. Aborting.\n");
-			free(decrypted);
-			return EXIT_FAILURE;
-		}
-		
-		for (i=0; i<strlen(decrypted); i++) {
-			fputc(decrypted[i], fp_rsa);
-		}
-		fclose(fp_rsa);
-		
-		// freeing
-		free(decrypted);
+		decrypt_file(argv[2]);
 	}
 	
 	// key pair generation
 	else if (strcmp(argv[1], "--generate-key-pair") == 0) {
-		int dir_exists;
-		mpz_t n, e, d;
-		
-		// checking .rsa existance
-		dir_exists = mkdir(".rsa", 0755);
-		
-		// .rsa exists
-		if (-1 == dir_exists) {
-			char user_choice;
-			printf("Directory exists. Generate new key pair? [y|n] ");
-			user_choice = getchar();
-			
-			if (user_choice != 'y' && user_choice != 'n') {
-				printf("Aborting.\n");
-				return EXIT_FAILURE;
-			}
-			
-			if ('y' == user_choice) {
-				// init
-				mpz_inits(n, e, d, NULL);
-				
-				// generating key pair
-				printf("Generating key pair...");
-				generate_keypair(n, e, d);
-				printf(" Done.\n");
-				
-				// saving
-				if (-1 == save_keypair(n, e, d)) {
-					mpz_clears(n, e, d, NULL);
-					return EXIT_FAILURE;
-				}
-				
-				// cleaning
-				mpz_clears(n, e, d, NULL);
-			}
-		}
-		
-		// .rsa doesn't exists
-		else {
-			// inits
-			mpz_inits(n, e, d, NULL);
-			
-			// generating
-			printf("Generating key pair...");
-			generate_keypair(n, e, d);
-			printf(" Done.\n");
-			
-			// saving
-			save_keypair(n, e, d);
-			
-			// cleaning
-			mpz_clears(n, e, d, NULL);
-		}
+		key_pair();
 	}
 	
 	// option not recognized
